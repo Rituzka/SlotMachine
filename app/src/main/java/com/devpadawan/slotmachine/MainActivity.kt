621b2f1b113af37353e1c4b7815b11e7735c666b
Rita <rituzka@gmail.com>
2021-05-31 00:08:06 +0200

    viewBinding implemented

--- app/src/main/java/com/devpadawan/slotmachine/MainActivity.kt
+++ app/src/main/java/com/devpadawan/slotmachine/MainActivity.kt
@@ -2,10 +2,33 @@ package com.devpadawan.slotmachine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.devpadawan.slotmachine.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val random = Random()
+    var slot1 = 0
+    var slot2 = 0
+    var slot3 = 0
+    var win = 10000
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener {
+           Toast.makeText(this, "You've spinned - 1000 euros discounted", Toast.LENGTH_SHORT).show()
+
+            binding.imgSlot1.setImageResource(R.drawable.animation)
+            binding.imgSlot2.setImageResource(R.drawable.animation)
+            binding.imgSlot3.setImageResource(R.drawable.animation)
+
+        }
+
     }
 }
